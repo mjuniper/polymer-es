f4a74d9179cf894e4553c19bed13f9fb1d2db19f
mjuniper <mjuniper@gmail.com>
2014-10-01 10:10:08 -0600

    item-details

--- app/elements/esri-search-result/esri-search-result.js
+++ app/elements/esri-search-result/esri-search-result.js
@@ -8,7 +8,7 @@
     },
 
     onItemClick: function (evt) {
-      this.fire('esri:search-result:item-clicked', this.item);
+      this.fire('esri:search-result:item-clicked', this.item._source);
     },
 
     stripHtml: function (html) {
