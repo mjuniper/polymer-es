00a666cb52efa05cadc379cad9c83b25118c1fca
mjuniper <mjuniper@gmail.com>
2014-10-02 18:24:25 -0600

    whole pile o changes

--- app/elements/esri-search-field/esri-search-field.js
+++ app/elements/esri-search-field/esri-search-field.js
@@ -11,11 +11,12 @@
 
     domReady: function () {
       //var url = document.querySelector(this.searchEl).url + '_search?size=5&q=tags:~QUERY~*%20title:~QUERY~*&_source_include=title,tags';
-      var url = document.querySelector(this.searchEl).url + '_search?source={"query":{"dis_max":{"queries":[{"match":{"title":{"query":"~QUERY~","operator":"and","boost":1,"analyzer":"od_autocomplete"}}},{"match":{"tags":{"query":"~QUERY~","operator":"and","boost":1,"analyzer":"od_autocomplete"}}}]}},"size":10,"from":0,"_source":["title","tags","name"]}';
+      var url = document.querySelector(this.searchEl).url;// + '_search';//?source={"query":{"dis_max":{"queries":[{"match":{"title":{"query":"~QUERY~","operator":"and","boost":1,"analyzer":"od_autocomplete"}}},{"match":{"tags":{"query":"~QUERY~","operator":"and","boost":1,"analyzer":"od_autocomplete"}}}]}},"size":10,"from":0,"_source":["title","tags","name"]}';
       this.initTypeahead(url);
     },
 
     onSubmit: function (e) {
+      //this causes an apparently inconsequential script error
       e.preventDefault();
       this.fire('esri:search:before-start', this.searchString);
       this.typeahead.typeahead('close');
@@ -27,10 +28,10 @@
       this.fire('esri:search:before-start', this.searchString);
     },
 
-    getTypeaheadUrl: function (url, query) {
-      url = url.replace(/~QUERY~/gi, query);
-      return url;
-    },
+    // getTypeaheadUrl: function (url, query) {
+    //   url = url.replace(/~QUERY~/gi, query);
+    //   return url;
+    // },
 
     initBloodhound: function (url) {
       var self = this;
@@ -40,7 +41,7 @@
         remote: {
           url: url,
           rateLimitWait: 150,
-          replace: self.getTypeaheadUrl,
+          replace: self.getTypeaheadUrl.bind(self),
           filter: function(response) {
             return response.hits.hits;
           }
@@ -73,6 +74,78 @@
       };
 
       this.typeahead = $(this.$.search).typeahead(opts, datasets).on('typeahead:selected', this.onTypeaheadSelected.bind(this));
+    },
+
+    getTypeaheadUrl: function (url, query) {
+      //clone it
+      var searchObj = JSON.parse(JSON.stringify(this.searchObj));
+
+      //add searchString
+      searchObj.query.filtered.query.dis_max.queries.forEach(function (item) {
+        if (item.match.title) {
+          item.match.title.query = query;  
+        }
+        if (item.match.tags) {
+          item.match.tags.query = query;  
+        }
+      });
+
+      //add extent if present
+      if (this.extent) {
+        var ext = this.extent;
+        searchObj.query.filtered.filter = {
+          bool: { 
+            must: [
+              { range: { min_y: { gte: ext.ymin, lte: ext.ymax } } },
+              { range: { max_y: { lte: ext.ymax, gte: ext.ymin } } },
+              { range: { min_x: { gte: ext.xmin, lte: ext.xmax } } },
+              { range: { max_x: { lte: ext.xmax, gte: ext.xmin } } }
+            ]
+          }
+        }
+      }
+
+      //add size
+      searchObj.size = this.size;
+
+      //construct the url and return it
+      return url + '_search?source=' + JSON.stringify(searchObj);
+    },
+
+    searchObj: {
+      query: {
+        filtered: {
+          query: {
+              dis_max: {
+                queries: [
+                  {
+                    match: {
+                      title: {
+                        query: '',
+                        operator: 'and',
+                        boost: 1,
+                        analyzer: 'od_autocomplete'
+                      }
+                    }
+                  },
+                  {
+                    match: {
+                      tags: {
+                        query: '',
+                        operator: 'and',
+                        boost: 1,
+                        analyzer: 'od_autocomplete'
+                      }
+                    }
+                  }
+                ]
+              }
+            },
+        }
+      },
+      from: 0,
+      size: 10,
+      _source: ['title', 'snippet', 'type','id','min_y','min_x','max_y','max_x']
     }
 
   });
--- app/elements/esri-search-result/esri-search-result.js
+++ app/elements/esri-search-result/esri-search-result.js
@@ -11,6 +11,16 @@
       this.fire('esri:search-result:item-clicked', this.item._source);
     },
 
+    onMouseOver: function (evt) {
+      evt.preventDefault();
+      this.fire('esri:search-result:item-mouseover', this.item._source);
+    },
+
+    onMouseOut: function (evt) {
+      evt.preventDefault();
+      this.fire('esri:search-result:item-mouseout', this.item._source);
+    },
+
     stripHtml: function (html) {
       html = html || '';
       html = (html === 'null') ? '' : html;
