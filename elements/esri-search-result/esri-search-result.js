941bd16e840a2c41647d6ffd764040cd606c6cc4
mjuniper <mjuniper@gmail.com>
2014-10-07 12:58:03 -0600

    Updates

--- elements/esri-search-result/esri-search-result.js
+++ elements/esri-search-result/esri-search-result.js
@@ -3,8 +3,26 @@
 
   Polymer({
 
+    showDefault: true,
+
     created: function () {
       this.item = {};
+    },
+
+    ready: function() {
+      //based on http://stackoverflow.com/questions/26125308/how-to-inject-template-into-polymer-component-by-providing-it-as-element-conte
+      var template = document.querySelector('template#' + this.itemTemplate);
+      if (template) {
+        // Allow Polymer expressions in the template's {{}}.
+        if (!template.bindingDelegate) {
+          template.bindingDelegate = this.element.syntax;
+        }
+
+        this.$['result-item-container'].appendChild(template.createInstance(this));
+        //this.injectBoundHTML(template.createInstance(this), this.$['result-item-container']);
+        
+        this.showDefault = false;
+      }
     },
 
     onItemClick: function (evt) {
